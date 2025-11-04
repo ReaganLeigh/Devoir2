594defc01760e0087e07161a3da79b0e4b229140
hezios571 <jayrome2002@hotmail.com>
2025-11-04 15:07:47 -0500

    generation neighbors

--- solver_advanced.py
+++ solver_advanced.py
@@ -1,9 +1,48 @@
-def solve(schedule):
-    """
-    Your solution of the problem
-    :param schedule: object describing the input
-    :return: a list of tuples of the form (c,t) where c is a course and t a time slot. 
-    """
-    # Add here your agent
+from schedule import Schedule
+from solver_naive import solve as naive_solve  
+import random
 
-    raise Exception("Agent is not implemented")
+
+def cost(schedule: Schedule, solution: dict) -> int:
+    return schedule.get_n_creneaux(solution)
+
+# solution naive
+def generate_initial_solution(schedule: Schedule) -> dict:
+    sol = naive_solve(schedule)   
+    schedule.verify_solution(sol) 
+    return sol
+
+
+def generate_neighbors(solution: dict, schedule: Schedule) -> list[dict]:
+    neighbors = []
+
+    courses = list(solution.keys())
+    course = random.choice(courses) # on prend randomly un cours
+
+    current_slot = solution[course]
+    used_slots = set(solution.values())
+
+    # on essaie tous les créneaux déjà utilisés
+    for slot in used_slots:
+        if slot == current_slot:
+            continue
+        new_sol = solution.copy()
+        new_sol[course] = slot      # essaie de mettre un cours dans un nouveau timeslot
+        neighbors.append(new_sol)   # retourne toutes les solutions possibles ou non
+
+    return neighbors
+
+
+
+
+
+def solve(schedule: Schedule):
+    S = generate_initial_solution(schedule)
+    S_best = S.copy()
+    best_cost = cost(schedule, S_best)
+
+    max_iter = 10000
+        
+
+    
+    return S_best
